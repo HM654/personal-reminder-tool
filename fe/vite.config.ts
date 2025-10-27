a872ae12fd63948497be23bc88023fbe7192955f
HM654 <harry.moore.dev@gmail.com>
2025-10-27 21:43:52 +0000

    fix: corrected base url

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/personal-reminder-tool-fe/',
+  base: '/personal-reminder-tool/',
   plugins: [
     vue(),
     vueDevTools(),
